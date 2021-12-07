2ddf2ee8d55b23afdea5caec475532d5d5569915
Michelle Wilber <46725544+mmwilber@users.noreply.github.com>
2021-12-07 12:24:39 -0900

    Update Inception_rate.py

--- Inception_rate.py
+++ Inception_rate.py
@@ -34,7 +34,10 @@ st.write("This is a calculator to calculate the effective per kWh rate when the 
 #utility = st.selectbox('Select a utility:', utilities ) #make a drop down list and get choice
 #tmyid = dfc['TMYid'].loc[dfc['aris_city']==city].iloc[0] #find the corresponding TMYid - OLD edit this to get rate info maybe
 
+st.write("")
+st.write("Note: you may use your computer's arrow keys with the mouse to do fine adjustments to the slider values below:")
 
+st.write("")
 dc = (st.slider('Enter the demand charge, in $/kW', max_value = 44.53, value = 20.0))
 ec = (st.slider('Enter the energy charge, in $/kWh', max_value = .6000, step = .0001, format = "%1.4f",value = .1200))
 lf = (st.slider('Enter the assumed load factor, in %', max_value = 100.0, step = .1, format = "%1.1f",value = 10.0))
@@ -42,11 +45,10 @@ lf = (st.slider('Enter the assumed load factor, in %', max_value = 100.0, step =
 # calculate the inception rate
 
 rate = dc/(lf/100 * 730) + ec
-
-st.write("Note: some utilities might have seasonal rates, which may neccessitate performing more than one calculation")
-
 st.write("")
 st.write("The inception rate per kWh is calculated as ", round(rate,5))
+st.write("")
+st.write("Note: some utilities might have seasonal rates, which may neccessitate performing more than one calculation")
 
 st.write("")
 st.write("Please peak under the hood at this code, which can be found at")
