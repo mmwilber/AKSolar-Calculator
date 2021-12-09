bfce3291d175bf72b83163a33808c6281cf6d088
Michelle Wilber <46725544+mmwilber@users.noreply.github.com>
2021-12-08 17:07:58 -0900

    Update Inception_rate.py

--- Inception_rate.py
+++ Inception_rate.py
@@ -22,49 +22,54 @@ st.write("where each utility would use the \
 current demand and energy charges approved in its last general rate case and propose and \
 support an assumed load factor as part of its tariff filing proposing a DCFC inception rate for RCA approval.")
 
-st.write("This is a calculator to calculate the effective per kWh rate when the other factors are input.")
+st.write("This is a calculator to calculate the effective per kWh inception rate, given an assumed load factor.")
 
 #upload a list of utilities to choose from:
 #upload the csv of rates
 # 
 df = pd.read_csv('utility_rates_12_8_21.csv')
-
-#now create a drop down menu of the available utilities 
-utilities = df['Utility'] #get a list of names
-utility = st.selectbox('Select a utility:', utilities ) #make a drop down list and get choice
-
 st.write("Rates in our database are valid on 12/8/21")
-custom = st.checkbox("Check here if your utility wasn't listed or you would like to use custom or updated rate")
-
-st.write("")
-if custom:
+one = st.checkbox("Check here if you want to look at a specific utility's inception rate, otherwise all the utilities on file will be compared.")
+if one:
   
-  st.write("Note: you may use your computer's arrow keys with the mouse to do fine adjustments to the slider values below:")
+  #now create a drop down menu of the available utilities 
+  utilities = df['Utility'] #get a list of names
+  utility = st.selectbox('Select a utility:', utilities ) #make a drop down list and get choice
+
+
+  custom = st.checkbox("Check here if your utility wasn't listed or you would like to use custom or updated rate")
 
   st.write("")
-  dc = (st.slider('Enter the demand charge, in $/kW', max_value = 44.53, value = 20.0))
-  ec = (st.slider('Enter the energy charge, in $/kWh', max_value = .6000, step = .0001, format = "%1.4f",value = .1200))
+  if custom:
   
+    st.write("Note: you may use your computer's arrow keys with the mouse to do fine adjustments to the slider values below:")
+
+    st.write("")
+    dc = (st.slider('Enter the demand charge, in $/kW', max_value = 44.53, value = 20.0))
+    ec = (st.slider('Enter the energy charge, in $/kWh', max_value = .6000, step = .0001, format = "%1.4f",value = .1200))
+  
+  else:
+    dc = df['LCD'].loc[df['Utility']==utility].iloc[0] #find the corresponding demand rate
+    ec = df['LCE'].loc[df['Utility']==utility].iloc[0] #find the corresponding energy rate
+  
+  lf = (st.slider('Enter the assumed load factor, in %', max_value = 100.0, step = .1, format = "%1.1f",value = 10.0))
+
+  # calculate the inception rate
+
+  rate = dc/(lf/100 * 730) + ec
+  st.write("")
+  st.write("The inception rate per kWh is calculated as ", round(rate,5))
+  st.write("")
+  st.write("Note: some utilities might have seasonal rates, which may neccessitate performing more than one calculation")
+
 else:
-  dc = df['LCD'].loc[df['Utility']==utility].iloc[0] #find the corresponding demand rate
-  ec = df['LCE'].loc[df['Utility']==utility].iloc[0] #find the corresponding energy rate
-  
-lf = (st.slider('Enter the assumed load factor, in %', max_value = 100.0, step = .1, format = "%1.1f",value = 10.0))
-
-# calculate the inception rate
-
-rate = dc/(lf/100 * 730) + ec
-st.write("")
-st.write("The inception rate per kWh is calculated as ", round(rate,5))
-st.write("")
-st.write("Note: some utilities might have seasonal rates, which may neccessitate performing more than one calculation")
-
-df['IR'] = df['LCD']/(lf/100 * 730) + df['LCE']
-tab = df[['Utility']]
-tab['Inception Rate'] = df.IR
-tab['Small Commercial Rate'] = df.SCE
-tab = tab.set_index('Utility')
-st.table(tab)
+  lf = (st.slider('Enter the assumed load factor, in %', max_value = 100.0, step = .1, format = "%1.1f",value = 10.0))
+  df['IR'] = df['LCD']/(lf/100 * 730) + df['LCE']
+  tab = df[['Utility']]
+  tab['Inception Rate'] = df.IR
+  tab['Small Commercial Rate'] = df.SCE
+  tab = tab.set_index('Utility')
+  st.table(tab)
 
 
 st.write("")
